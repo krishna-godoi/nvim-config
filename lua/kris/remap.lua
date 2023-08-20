e6c76a65063997f0ad2f1fd68c43008c4faacba8
krishna.godoi <krishna.godoi@scandiweb.com>
2023-08-20 21:27:46 +0300

    Removed unwanted keybinds for quickfix and location lists to enable tmux-navigation

--- lua/kris/remap.lua
+++ lua/kris/remap.lua
@@ -20,17 +20,14 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>r", [[:%s/]])
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.c.onfig/nvim/lua/kris/packer.lua<CR>");
 
 vim.keymap.set("n", "<leader>gs", ":Git \n" )
+vim.keymap.set("n", "x", '"_x')
